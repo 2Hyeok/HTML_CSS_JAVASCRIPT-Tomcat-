c55a2ea0f9805efcc9929f7414261e91d9f33522
2Hyeok <87698248+2Hyeok@users.noreply.github.com>
2022-06-08 19:16:32 +0900

    Add files via upload

---
+++ SimpleBean.java
@@ -0,0 +1,34 @@
+package bean;
+
+public class SimpleBean {
+	private String name;
+	private int age;
+	private String tel;
+	private String address;
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public String getAddress() {
+		return address;
+	}
+}
