3cf0d1e9e18fdce5c187e341852b2fac1b2c7caf
2Hyeok <87698248+2Hyeok@users.noreply.github.com>
2022-06-27 19:16:28 +0900

    Add files via upload

---
+++ memberEx/LogonDataBean.java
@@ -0,0 +1,65 @@
+package member;
+
+import java.sql.Timestamp;
+
+public class LogonDataBean {
+	private String id;
+	private String passwd;
+	private String name;
+	private String jumin1;
+	private String jumin2;
+	private String tel;
+	private String email;
+	private Timestamp reg_date;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPasswd() {
+		return passwd;
+	}
+	public void setPasswd(String passwd) {
+		this.passwd = passwd;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getJumin1() {
+		return jumin1;
+	}
+	public void setJumin1(String jumin1) {
+		this.jumin1 = jumin1;
+	}
+	public String getJumin2() {
+		return jumin2;
+	}
+	public void setJumin2(String junin2) {
+		this.jumin2 = junin2;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Timestamp getReg_date() {
+		return reg_date;
+	}
+	public void setReg_date(Timestamp reg_date) {
+		this.reg_date = reg_date;
+	}
+	
+}
+// DTO 저장빈
